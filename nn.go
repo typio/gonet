8e58c23ab786cc492167dbf6d98d9dd85924df79
Thomas Huber <tomsemail2100@gmail.com>
2018-04-15 18:26:23 -0700

    finished train()

--- nn.go
+++ nn.go
@@ -7,6 +7,7 @@ type NeuralNetwork struct {
 	inputNodes, hiddenNodes, outputNodes int
 	weightsIH, weightsHO                 *Matrix
 	biasH, biasO                         *Matrix
+	learningRate                         float64
 }
 
 // NewNN initializes network
@@ -21,14 +22,23 @@ func NewNN(inputNodes, hiddenNodes, outputNodes int) *NeuralNetwork {
 	biasH.Randomize()
 	biasO.Randomize()
 
+	learningRate := 0.1
+
 	return &NeuralNetwork{inputNodes, hiddenNodes, outputNodes,
 		weightsIH, weightsHO,
-		biasH, biasO}
+		biasH, biasO,
+		learningRate}
 }
 
 // Sigmoid is the logistic function
 func Sigmoid(x float64) float64 {
 	return 1 / (1 + math.Exp(-x))
+}
+
+// DSigmoid is the derivative of the logistic function and is run after the Sigmoid func
+func DSigmoid(y float64) float64 {
+	// return Sigmoid(x) * (1 - Sigmoid(x))
+	return y * (1 - y)
 }
 
 // FeedForward does the processess of the network
@@ -39,16 +49,57 @@ func (nn *NeuralNetwork) FeedForward(inputs *Matrix) *Matrix {
 	hidden.MapM(Sigmoid)
 
 	output := nn.weightsHO.MatrixP(hidden)
+	output.AddM(nn.biasO)
 	output.MapM(Sigmoid)
 
 	return output
 }
 
-// // Train uses backprop to modify weights
-// func (nn *NeuralNetwork) Train(inputs *Matrix, targets *Matrix) {
-// 	outputs := nn.FeedForward(inputs)
+// Train uses backprop to modify weights
+func (nn *NeuralNetwork) Train(inputs *Matrix, targets *Matrix) {
+	//output := nn.FeedForward(inputs)
+	hidden := nn.weightsIH.MatrixP(inputs)
+	hidden.AddM(nn.biasH)
+	hidden.MapM(Sigmoid)
 
-// 	// Calculate error
-// 	// ERROR = TARGETS - OUTPUTS
-// 	error := targets.SubtractM(outputs)
-// }
+	output := nn.weightsHO.MatrixP(hidden)
+	output.AddM(nn.biasO)
+	output.MapM(Sigmoid)
+
+	// Calculate error
+	// ERROR = TARGETS - OUTPUTS
+	outputErrors := targets.SubtractM(output)
+
+	// Calculate gradient
+	gradients := output.MapNM(DSigmoid)
+	gradients.MultiplyM(outputErrors)
+	gradients.Multiply(nn.learningRate)
+
+	// Calculate deltas
+	hiddenT := hidden.Transpose()
+	weightsHOD := gradients.MultiplyM(hiddenT)
+
+	// Adjust weights by deltas
+	nn.weightsHO.AddM(weightsHOD)
+	// Adjusts the bias by gradients
+	nn.biasO.AddM(gradients)
+
+	// Calculate hidden layer errors
+	weightsHOT := nn.weightsHO.Transpose()
+	hiddenErrors := weightsHOT.MultiplyM(outputErrors)
+
+	// Calculate hidden gradient
+	hiddenGradient := hidden.MapNM(DSigmoid)
+	hiddenGradient.MultiplyM(hiddenErrors)
+	hiddenGradient.Multiply(nn.learningRate)
+
+	// Calculate input -> deltas
+	inputsT := inputs.Transpose()
+	weightsIHD := hiddenGradient.MultiplyM(inputsT)
+
+	nn.weightsIH.AddM(weightsIHD)
+	// Adjusts the bias by gradients
+	nn.biasH.AddM(hiddenGradient)
+
+	output.PrintM()
+}
